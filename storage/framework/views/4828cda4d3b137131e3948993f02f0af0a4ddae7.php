85064d48c8f1223b18c7f78c972cd5c280917e00
Md. Naimuzzaman Fahim <naimuzzamanfahim17@gmail.com>
2025-01-01 01:02:12 +0600

    base

---
+++ storage/framework/views/4828cda4d3b137131e3948993f02f0af0a4ddae7.php
@@ -0,0 +1,10 @@
+  <!-- Banner Area Starts -->
+  <section id="home" class="w-full h-screen flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat" style="background-image: url(<?php echo e($bannerImg); ?>)">
+    
+    <h1 class="font-cursive-merie text-white text-center leading-normal text-6xl">
+      Discover the <span class="text-amber-400">flavors</span><br />
+      of <span class="text-amber-400">Dine</span>Sphere
+    </h1>
+  </section>
+  <!-- Banner Area End -->
+<?php /**PATH G:\BRACU\10th Semester (Fall-24)\CSE470\Project\Restaurant_Management-Laravel-Project\resources\views/home/partials/banner.blade.php ENDPATH**/ ?>
